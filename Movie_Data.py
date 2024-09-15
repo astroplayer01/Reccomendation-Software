97f323f995ba19897106452f052b034ee469fc5a
astroplayer01 <natearends04@gmail.com>
2024-09-15 14:24:17 -0500

    created the movie data file which holds all of the movies and certain information about them

---
+++ Movie_Data.py
@@ -0,0 +1,24 @@
+genres = ['drama', 'crime', 'action', 'thriller', 'biography', 'adventure', 'sci-fi', 'mystery', 'war', 'fantasy']
+
+movie_data = [
+    ['drama', 'The Shawshank Redemption', '9.3', '142'],
+    ['crime', 'The Godfather', '9.2', '175'],
+    ['action', 'The Dark Knight', '9.0', '152'],
+    ['thriller', 'Pulp Fiction', '8.9', '154'],
+    ['biography', 'Schindler\'s List', '8.9', '195'],
+    ['adventure', 'The Lord of the Rings', '8.8', '178'],
+    ['drama', 'Fight Club', '8.8', '139'],
+    ['drama', 'Forrest Gump', '8.8', '142'],
+    ['sci-fi', 'Inception', '8.7', '148'],
+    ['sci-fi', 'The Matrix', '8.7', '136'],
+    ['crime', 'Goodfellas', '8.7', '146'],
+    ['thriller', 'The Silence of the Lambs', '8.6', '118'],
+    ['thriller', 'Se7en', '8.6', '127'],
+    ['mystery', 'The Usual Suspects', '8.5', '106'],
+    ['war', 'Saving Private Ryan', '8.5', '169'],
+    ['sci-fi', 'Interstellar', '8.6', '169'],
+    ['drama', 'The Green Mile', '8.6', '189'],
+    ['action', 'Gladiator', '8.5', '155'],
+    ['mystery', 'The Prestige', '8.5', '130'],
+    ['crime', 'The Departed', '8.5', '151']
+]
