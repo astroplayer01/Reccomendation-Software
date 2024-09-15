d36abde3759b9c94c3b832714bc599ba70ae1ed4
astroplayer01 <natearends04@gmail.com>
2024-09-15 15:23:51 -0500

    created teh user prompt

--- Inverted_Index_Hash.py
+++ Inverted_Index_Hash.py
@@ -9,44 +9,48 @@ length_index = {}
 
 
 #puts the data into the table
-for movie in movie_data:
-    #declares variables with each data point
-    genre, name, rating, length = movie
+def hash():
+    global genre_index, name_index, rating_index, length_index
+    for movie in movie_data:
+        #declares variables with each data point
+        genre, name, rating, length = movie
 
-    #creates the key if not already existing then inserts the 2d index there
-    if genre not in genre_index:
-        genre_index[genre] = []
+        #creates the key if not already existing then inserts the 2d index there
+        if genre not in genre_index:
+            genre_index[genre] = []
 
-    genre_index[genre].append(movie)
+        genre_index[genre].append(movie)
 
-    if name not in name_index:
-        name_index[name] = []
+        if name not in name_index:
+            name_index[name] = []
         
-    name_index[name].append(movie)
+        name_index[name].append(movie)
 
-    rating = float(rating)
-    if rating not in rating_index:
-        rating_index[rating] = []
+        rating = float(rating)
+        if rating not in rating_index:
+            rating_index[rating] = []
         
-    rating_index[rating].append(movie)
+        rating_index[rating].append(movie)
 
-    length = int(length)
-     #using a range for the movie length
-    length_range = f"{(length // 10) * 10}-{((length // 10) + 1) * 10}"
-    if length_range not in length_index:
-        length_index[length_range] = []
+        length = int(length)
+        #using a range for the movie length
+        length_range = f"{(length // 10) * 10}-{((length // 10) + 1) * 10}"
+        if length_range not in length_index:
+            length_index[length_range] = []
         
-    length_index[length_range].append(movie)
+        length_index[length_range].append(movie)
 
 #querey the indexes
-def find_movies_by_genre(genre):
-    return genre_index.get(genre, [])
+    def find_movies_by_genre(genre):
+        return genre_index.get(genre, [])
 
-def find_movies_by_rating(rating):
-    return rating_index.get(rating, [])
+    def find_movies_by_rating(rating):
+        return rating_index.get(rating, [])
 
-def find_movies_by_length(length_range):
-    return length_index.get(length_range, [])
+    def find_movies_by_length(length_range):
+        return length_index.get(length_range, [])
 
-def find_movie_by_name(name):
-    return name_index.get(name, None)
+    def find_movie_by_name(name):
+        return name_index.get(name, None)
+
+
--- Reccomendation_Software.py
+++ Reccomendation_Software.py
@@ -1,1 +1,31 @@
-import Inverted_Index_Hash
+from Inverted_Index_Hash import hash
+
+def Reccomendation_Software():
+    welcome_message()
+
+
+
+def welcome_message():
+    message = """
+    **************************************************
+    *                                                *
+    *                WELCOME TO OUR                  *
+    *                                                *
+    *                  MOVIE APP!                    *
+    *                                                *
+    *        We hope you find the perfect movie!     *
+    *                                                *
+    **************************************************
+    """
+    print(message)
+
+def user_input():
+    
+    print()
+    user_prompt = input("Please enter either the Name, Genre, Rating, or Length of the movie you want to find: ")
+
+    if type(user_input) == str:
+        pass
+
+
+print("finish")
