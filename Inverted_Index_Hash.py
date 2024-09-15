430b32343a9b14b887f8e1af04f1661b014cca23
astroplayer01 <natearends04@gmail.com>
2024-09-15 15:02:53 -0500

    made some fixes

--- Inverted_Index_Hash.py
+++ Inverted_Index_Hash.py
@@ -1,4 +1,4 @@
-import Movie_Data
+from Movie_Data import movie_data
 
 #creates the dictionaries to house the data at certain indexes
 genre_index = {}
@@ -9,36 +9,34 @@ length_index = {}
 
 
 #puts the data into the table
-def set_hash():
+for movie in movie_data:
+    #declares variables with each data point
+    genre, name, rating, length = movie
 
-    for movie in Movie_Data:
-        #declares variables with each data point
-        genre, name, rating, length = movie
+    #creates the key if not already existing then inserts the 2d index there
+    if genre not in genre_index:
+        genre_index[genre] = []
 
-        #creates the key if not already existing then inserts the 2d index there
-        if genre not in genre_index:
-            genre_index[genre] = []
+    genre_index[genre].append(movie)
 
-        genre_index[genre].append(movie)
-
-        if name not in name_index:
-            name_index[name] = []
+    if name not in name_index:
+        name_index[name] = []
         
-        name_index[name].append(movie)
+    name_index[name].append(movie)
 
-        rating = float(rating)
-        if rating not in rating_index:
-            rating_index[rating] = []
+    rating = float(rating)
+    if rating not in rating_index:
+        rating_index[rating] = []
         
-        rating_index[rating].append(movie)
+    rating_index[rating].append(movie)
 
-        length = int(length)
-        #using a range for the movie length
-        length_range = f"{(length // 10) * 10}-{((length // 10) + 1) * 10}"
-        if length_range not in length_index:
-            length_index[length_range] = []
+    length = int(length)
+     #using a range for the movie length
+    length_range = f"{(length // 10) * 10}-{((length // 10) + 1) * 10}"
+    if length_range not in length_index:
+        length_index[length_range] = []
         
-        length_index[length_range].append(movie)
+    length_index[length_range].append(movie)
 
 #querey the indexes
 def find_movies_by_genre(genre):
@@ -52,6 +50,3 @@ def find_movies_by_length(length_range):
 
 def find_movie_by_name(name):
     return name_index.get(name, None)
-
-
-
